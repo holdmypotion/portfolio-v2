a1b6c40c77c781402fcab5b99b62973158513388
holdmypotion <holdmypotion@gmail.com>
2021-05-03 19:33:17 +0530

    Added floating headers in the hero section

--- components/home/heroSection.js
+++ components/home/heroSection.js
@@ -1,8 +1,14 @@
+import styles from "../../styles/heroSection.module.css";
+
 const HeroSection = () => {
   return (
-    <div style={{ backgroundColor: "#F4955C", height: "1000vh" }}>
-      <h1>Developer</h1>
-      <h1>DESIGNER</h1>
+    <div style={{ backgroundColor: "#F4955C", height: "100vh" }}>
+      <div className={styles.headerContainer}>
+        <div className={styles.floatingHeader}>
+          <h1 className={styles.developer}>Developer.</h1>
+          <h1 className={styles.designer}>Designer.</h1>
+        </div>
+      </div>
     </div>
   );
 };
