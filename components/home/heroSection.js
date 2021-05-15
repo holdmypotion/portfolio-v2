43bd4e47c8542d4941c7e676dfad53ea3c2a454e
Rahul Sharma <holdmypotion@gmail.com>
2021-05-15 05:43:25 +0530

    Added placeholder hero image

--- components/home/heroSection.js
+++ components/home/heroSection.js
@@ -12,17 +12,16 @@ const HeroSection = () => {
           <h1 className={styles.designer}>Designer.</h1>
         </div>
       </div>
-      <div className={styles.imageContainer}>
-        {/* TODO: Get the hero image sorted */}
-        {/* <Image
-          src="/images/portrait-2.png"
-          width={863}
-          height={1590}
-          // quality={100}
-          // layout="fill"
-          // objectFit="fill"
-          // objectPosition="bottom"
-        /> */}
+      <div className={styles.imageContainerWrapper}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/images/portrait-new-nc.png"
+            quality={100}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="top"
+          />
+        </div>
       </div>
       <div className={styles.introText}>
         <IntroText />
