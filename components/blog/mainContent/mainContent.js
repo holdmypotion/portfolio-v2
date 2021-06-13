9ea9bc07def1ae1bf0203cb4d6bfa554b386d593
Rahul Sharma <holdmypotion@gmail.com>
2021-06-13 09:18:41 +0530

    Fixed sticky container problem

--- components/blog/mainContent/mainContent.js
+++ components/blog/mainContent/mainContent.js
@@ -1,13 +1,10 @@
-import { Container } from '../../../styles/blog/globalStyles';
 import { ContentContainer } from '../../../styles/blog/mainContentStyles';
 import Blogs from './blogs';
 
 export default function mainContent() {
   return (
-    <Container>
-      <ContentContainer>
-        <Blogs />
-      </ContentContainer>
-    </Container>
+    <ContentContainer>
+      <Blogs />
+    </ContentContainer>
   );
 }
