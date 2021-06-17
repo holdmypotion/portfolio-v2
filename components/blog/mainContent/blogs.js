1643ce8ffc00a0dfa4c195b5cf26421b1b169398
Rahul Sharma <holdmypotion@gmail.com>
2021-06-17 09:33:21 +0530

    deploy fix

--- components/blog/mainContent/blogs.js
+++ components/blog/mainContent/blogs.js
@@ -9,47 +9,49 @@ export default function Blogs({ blogs }) {
   // TODO: Set the excerpt from the body of the article according to the type.
   const searchContext = useContext(SearchContext);
   const { tags } = useContext(FilterContext);
-  let filteredArticles;
+  let filteredArticles = null;
+  if (blogs) {
+    filteredArticles = blogs.filter(article => {
+      if (tags.length === 0) {
+        return article;
+      }
+      if (tags.some(val => article.fields.tags.includes(val))) {
+        return article;
+      } else {
+        return null;
+      }
+    });
 
-  filteredArticles = blogs.filter(article => {
-    if (tags.length === 0) {
-      return article;
-    }
-    if (tags.some(val => article.fields.tags.includes(val))) {
-      return article;
-    } else {
-      return null;
-    }
-  });
-
-  filteredArticles = filteredArticles.filter(article => {
-    if (article.fields.title.toLowerCase().includes(searchContext.query)) {
-      return article;
-    } else {
-      return null;
-    }
-  });
+    filteredArticles = filteredArticles.filter(article => {
+      if (article.fields.title.toLowerCase().includes(searchContext.query)) {
+        return article;
+      } else {
+        return null;
+      }
+    });
+  }
 
   return (
     <Container>
-      {filteredArticles.map((article, index) => {
-        const data = article.fields;
-        const date = data.publishDate.slice(0, 10);
-        console.log(date);
-        const types = ['small', 'large', 'medium'];
-        const type = types[index % 3];
-        return (
-          <BlogThumb
-            key={data.slug}
-            type={type}
-            title={data.title}
-            excerpt={data.description}
-            comments={data.comments.comments}
-            date={date}
-            image={data.featuredImage}
-          />
-        );
-      })}
+      {filteredArticles !== null &&
+        filteredArticles.map((article, index) => {
+          const data = article.fields;
+          const date = data.publishDate.slice(0, 10);
+          console.log(date);
+          const types = ['small', 'large', 'medium'];
+          const type = types[index % 3];
+          return (
+            <BlogThumb
+              key={data.slug}
+              type={type}
+              title={data.title}
+              excerpt={data.description}
+              comments={data.comments.comments}
+              date={date}
+              image={data.featuredImage}
+            />
+          );
+        })}
     </Container>
   );
 }
