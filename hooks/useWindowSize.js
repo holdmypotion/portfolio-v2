b10b79577e5ebe65afae59d55701c558fe500333
Rahul Sharma <holdmypotion@gmail.com>
2021-07-16 23:05:21 +0530

    Fixed Panel height glicth

--- hooks/useWindowSize.js
+++ hooks/useWindowSize.js
@@ -14,6 +14,7 @@ export default function useWindowSize() {
       setWindowSize(getSize());
     };
 
+    handleResize();
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
